a0d65491d7df287ab8e70d9bd5c9ba875eac0bcb
Antonio Ribeiro <antonioribeiro.me@gmail.com>
2018-02-27 19:42:13 -0300

    Add Project
    This is the first commit of the project.
    
    - SplashScreen with Feira Preta's logo
    
    - Main Activity with "Hello World" message

---
+++ app/src/main/java/br/com/feirapreta/activities/MainActivity.java
@@ -0,0 +1,17 @@
+package br.com.feirapreta.activities;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.feirapreta.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
