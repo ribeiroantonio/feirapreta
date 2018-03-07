2a8ad306090e6aa782e835961bc0febdb1ef4aa0
Antonio Ribeiro <antonioribeiro.me@gmail.com>
2018-03-06 21:38:15 -0300

    Add Post Method

--- app/src/main/java/br/com/feirapreta/activities/MainActivity.java
+++ app/src/main/java/br/com/feirapreta/activities/MainActivity.java
@@ -1,23 +1,26 @@
 package br.com.feirapreta.activities;
 
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 import br.com.feirapreta.R;
 import br.com.feirapreta.adapter.HighlightsAdapter;
-import br.com.feirapreta.model.HighlightService;
-import br.com.feirapreta.model.Person;
+import br.com.feirapreta.model.RetrofitService;
 import br.com.feirapreta.model.Post;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -25,16 +28,14 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private RecyclerView recyclerView;
     private ArrayList<Post> highlights;
     private RecyclerView.Adapter highlightsAdapter;
     private RecyclerView.LayoutManager layoutManager;
     private SwipeRefreshLayout swipeRefreshLayout;
-
-    //TAG DO ERRO
-    public static final String TAG = "DEU ERRO";
+    private EditText editTextSearch;
 
     //TOKEN AUTENTICAÇÂO
     private static final String TOKEN = "OTOKENFICAAQUI";
@@ -43,30 +44,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Fresco.initialize(this);
+
         initViews();
 
         //SharedPreferences preferences = getSharedPreferences(getString(R.string.token), 0);
     }
 
     private void initViews(){
+        editTextSearch = findViewById(R.id.editText_search);
         recyclerView = findViewById(R.id.recyclerview);
 
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setItemViewCacheSize(20);
-        recyclerView.setDrawingCacheEnabled(true);
-        recyclerView.setDrawingCacheQuality(View.DRAWING_CACHE_QUALITY_HIGH);
-        layoutManager = new GridLayoutManager(this, 2);
-        recyclerView.setLayoutManager(layoutManager);
-        highlights = new ArrayList<>();
-        highlights.add(new Post("", "", true, "", "", "", "", new Person("", "carregando...", "", "")));
-        highlights.add(highlights.get(0));
-        highlights.add(highlights.get(0));
-        highlights.add(highlights.get(0));
-        highlights.add(highlights.get(0));
-        highlights.add(highlights.get(0));
-        highlightsAdapter = new HighlightsAdapter(highlights);
-        recyclerView.setAdapter(highlightsAdapter);
+        loadSearchBar();
+        loadRVHighLights();
         loadHighlights();
 
         swipeRefreshLayout = findViewById(R.id.swipeRefreshHomeScreen);
@@ -79,30 +70,79 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void loadHighlights(){
+        
+        if(isNetworkAvailable()){
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl(RetrofitService.BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
 
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(HighlightService.BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
+            RetrofitService request = retrofit.create(RetrofitService.class);
+            Call<ArrayList<Post>> call = request.listHighlight();
+            call.enqueue(new Callback<ArrayList<Post>>() {
+                @Override
+                public void onResponse(Call<ArrayList<Post>> call, Response<ArrayList<Post>> response) {
 
-        HighlightService request = retrofit.create(HighlightService.class);
-        Call<ArrayList<Post>> call = request.listHighlight();
-        call.enqueue(new Callback<ArrayList<Post>>() {
+                    highlights = response.body();
+                    highlightsAdapter = new HighlightsAdapter(highlights);
+                    recyclerView.setAdapter(highlightsAdapter);
+                }
+
+                @Override
+                public void onFailure(Call<ArrayList<Post>> call, Throwable t) {
+                    Toast.makeText(MainActivity.this, "Houve um erro ao tentar se conectar com os nossos servidores!", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+        }else{
+            Toast.makeText(this, "Verifique sua conexão com a Internet", Toast.LENGTH_SHORT).show();
+        }
+
+        if(swipeRefreshLayout != null){
+            swipeRefreshLayout.setRefreshing(false);
+        }
+
+        
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(this.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+    private void loadSearchBar(){
+
+        editTextSearch.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
-            public void onResponse(Call<ArrayList<Post>> call, Response<ArrayList<Post>> response) {
-
-                highlights = response.body();
-                highlightsAdapter = new HighlightsAdapter(highlights);
-                recyclerView.setAdapter(highlightsAdapter);
-                swipeRefreshLayout.setRefreshing(false);
-
-            }
-
-            @Override
-            public void onFailure(Call<ArrayList<Post>> call, Throwable t) {
-                swipeRefreshLayout.setRefreshing(false);
-                Toast.makeText(MainActivity.this, "Houve um erro ao tentar se conectar com os nossos servidores!", Toast.LENGTH_SHORT).show();
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                if (i == EditorInfo.IME_ACTION_SEARCH) {
+                    Toast.makeText(MainActivity.this, "OEEEEEE", Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+                return false;
             }
         });
+    }
+
+    private void loadRVHighLights(){
+
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setItemViewCacheSize(20);
+        recyclerView.setDrawingCacheEnabled(true);
+        recyclerView.setDrawingCacheQuality(View.DRAWING_CACHE_QUALITY_HIGH);
+        layoutManager = new GridLayoutManager(this, 2);
+        recyclerView.setLayoutManager(layoutManager);
+
+        highlights = new ArrayList<>();
+
+        highlightsAdapter = new HighlightsAdapter(highlights);
+        recyclerView.setAdapter(highlightsAdapter);
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
--- app/src/main/java/br/com/feirapreta/model/RetrofitService.java
+++ app/src/main/java/br/com/feirapreta/model/RetrofitService.java
@@ -4,13 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Field;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 /**
  * Created by HyanSiqueira on 01/03/2018.
  */
 
-public interface HighlightService {
+public interface RetrofitService {
     //Escreve aqui o IP que estará o back
     String BASE_URL="http://feirapreta-001-site1.ctempurl.com/api/";
 
@@ -18,4 +20,7 @@ public interface HighlightService {
     // no @GET () entre parenteses coloca o final do ip por exemplo o i /highlights para pegar os highlights
     @GET("publication")
     Call<ArrayList<Post>> listHighlight();
+
+    @POST("endpoint")
+    Call<ArrayList<Post>> searchByTag(@Field("tag") String tag);
 }
