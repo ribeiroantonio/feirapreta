55b168e1a27ee0660c77f05f27bee8f7f35f8b27
Antonio Ribeiro <antonioribeiro.me@gmail.com>
2018-03-15 21:54:06 -0300

    Add EndlessScroll on RecyclerView

--- app/src/main/java/br/com/feirapreta/activities/SearchResultsActivity.java
+++ app/src/main/java/br/com/feirapreta/activities/SearchResultsActivity.java
@@ -3,10 +3,11 @@ package br.com.feirapreta.activities;
 import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.KeyEvent;
@@ -18,20 +19,13 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import br.com.feirapreta.R;
 import br.com.feirapreta.adapter.PostsAdapter;
 import br.com.feirapreta.model.Post;
-import br.com.feirapreta.model.RetrofitService;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class SearchResultsActivity extends AppCompatActivity {
 
@@ -43,6 +37,9 @@ public class SearchResultsActivity extends AppCompatActivity {
     private EditText editTextSearch;
     private String searchedText;
     private boolean isDemand;
+    private boolean isScrolling = false;
+    private int currentItems, totalItems, scrolledOutItems;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,17 +54,17 @@ public class SearchResultsActivity extends AppCompatActivity {
 
     }
 
-    private void initViews(){
+    private void initViews() {
 
         editTextSearch = findViewById(R.id.editText_searchScreen);
         Bundle bundle = getIntent().getExtras();
-        if(bundle.get("searchedText") != null){
+        if (bundle.get("searchedText") != null) {
             searchedText = bundle.getString("searchedText");
             editTextSearch.setText(searchedText);
         }
 
         recyclerView = findViewById(R.id.rvSearchResults);
-        
+
         loadSearchBar();
         loadRVPosts();
         loadPosts();
@@ -76,13 +73,18 @@ public class SearchResultsActivity extends AppCompatActivity {
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
-                loadPosts();
+                if (isDemand) {
+                    loadPostsByDemand();
+                    swipeRefreshLayout.setRefreshing(false);
+                } else {
+                    loadPosts();
+                }
             }
         });
 
     }
-    
-    private void loadSearchBar(){
+
+    private void loadSearchBar() {
         editTextSearch.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
@@ -96,7 +98,7 @@ public class SearchResultsActivity extends AppCompatActivity {
         });
     }
 
-    private void loadRVPosts(){
+    private void loadRVPosts() {
         recyclerView.setHasFixedSize(true);
 
         layoutManager = new GridLayoutManager(this, 3);
@@ -106,10 +108,48 @@ public class SearchResultsActivity extends AppCompatActivity {
 
         postsAdapter = new PostsAdapter(posts);
         recyclerView.setAdapter(postsAdapter);
+        if(isDemand){
+            recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+                @Override
+                public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                    super.onScrollStateChanged(recyclerView, newState);
+                    if (newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL) {
+                        isScrolling = true;
+                    }
+                }
+
+                @Override
+                public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                    super.onScrolled(recyclerView, dx, dy);
+                    currentItems = layoutManager.getChildCount();
+                    totalItems = layoutManager.getItemCount();
+                    scrolledOutItems = ((GridLayoutManager) layoutManager).findFirstVisibleItemPosition();
+
+                    if (isScrolling && (currentItems + scrolledOutItems == totalItems)) {
+                        isScrolling = false;
+                        loadPostsByDemand();
+                    }
+                }
+            });
+        }
+
     }
 
-    private void loadPosts(){
-        /*final Post post = new Post("https://scontent.cdninstagram.com/vp/46262dc93aae3c922a74d88776f62760/5B30C520/t51.2885-15/e35/p320x320/24838741_383209202121068_6888092107773313024_n.jpg", "https://www.instagram.com/p/BcXJ7-whIi-/");
+    private void loadPostsByDemand() {
+        new Handler().postDelayed(new Runnable() {
+            Random rand = new Random();
+            @Override
+            public void run() {
+                for (int i = 0; i < 12; i++){
+                    posts.add(posts.get(rand.nextInt(3 - 0) + 0));
+                    postsAdapter.notifyDataSetChanged();
+                }
+            }
+        }, 1000);
+    }
+
+    private void loadPosts() {
+        final Post post = new Post("https://scontent.cdninstagram.com/vp/46262dc93aae3c922a74d88776f62760/5B30C520/t51.2885-15/e35/p320x320/24838741_383209202121068_6888092107773313024_n.jpg", "https://www.instagram.com/p/BcXJ7-whIi-/");
         Post post1 = new Post("https://scontent.cdninstagram.com/vp/e5c226b97d04c9421b3cc404f0570b7e/5B43A51A/t51.2885-15/e35/p320x320/24126586_192408241337439_1892377936535748608_n.jpg", "https://www.instagram.com/p/BcDlpH2BB5a/");
         Post post2 = new Post("https://scontent.cdninstagram.com/vp/6f9803b2e13763cc0e9b996b461ec37e/5B27940C/t51.2885-15/e35/p320x320/23823410_1488491477900574_1307807199350751232_n.jpg", "https://www.instagram.com/p/BbsgpryB0fv/");
         Random rand = new Random();
@@ -117,11 +157,11 @@ public class SearchResultsActivity extends AppCompatActivity {
         posts.add(post1);
         posts.add(post2);
 
-        for (int i = 0; i < 18; i++){
+        for (int i = 0; i < 15; i++) {
             posts.add(posts.get(rand.nextInt(3 - 0) + 0));
-        }*/
+        }
 
-        if(isNetworkAvailable()){
+        /*if(isNetworkAvailable()){
             Gson gson = new GsonBuilder()
                     .setLenient()
                     .create();
@@ -168,7 +208,7 @@ public class SearchResultsActivity extends AppCompatActivity {
 
         if (swipeRefreshLayout != null){
             swipeRefreshLayout.setRefreshing(false);
-        }
+        }*/
 
     }
 
@@ -179,7 +219,7 @@ public class SearchResultsActivity extends AppCompatActivity {
         return activeNetworkInfo != null && activeNetworkInfo.isConnected();
     }
 
-    public void goback(View view){
+    public void goback(View view) {
         onBackPressed();
         finish();
     }
--- app/src/main/java/br/com/feirapreta/adapter/PostsAdapter.java
+++ app/src/main/java/br/com/feirapreta/adapter/PostsAdapter.java
@@ -7,8 +7,6 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.Toast;
 
 import com.facebook.drawee.view.SimpleDraweeView;
 
