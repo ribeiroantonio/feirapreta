a58eb9d460e959f761dacac3f754bb4e81456a59
hsiq <hyan.siqueira@gmail.com>
2018-04-02 13:19:30 -0300

    Dialog
    Dialog confirm

--- app/src/main/java/br/com/feirapreta/activities/VotingActivity.java
+++ app/src/main/java/br/com/feirapreta/activities/VotingActivity.java
@@ -1,5 +1,8 @@
 package br.com.feirapreta.activities;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -32,47 +35,76 @@ public class VotingActivity extends AppCompatActivity {
 
 
 
+
     public void onClick(View v) {
-        ratingBar = findViewById(R.id.ratingBar);
-        Float value = ratingBar.getRating();
-        voto.setValue(value);
-        dialog = new ProgressDialog(VotingActivity.this);
-        dialog.setMessage("Carregando...");
-        dialog.setCancelable(false);
-        dialog.show();
-        RetrofitService retrofitService = RetrofitService.retrofit.create(RetrofitService.class);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle("Confirmar Voto");
+        builder.setMessage("Você em certeza que deseja dar essa nota?");
+
+        builder.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+
+            public void onClick(DialogInterface dialog, int which) {
+
+
+                ratingBar = findViewById(R.id.ratingBar);
+                Float value = ratingBar.getRating();
+                voto.setValue(value);
+                dialog = new ProgressDialog(VotingActivity.this);
+
+                RetrofitService retrofitService = RetrofitService.retrofit.create(RetrofitService.class);
 
 
 
 
-        final Call<ResponseBody> call = retrofitService.voting(voto);
+                final Call<ResponseBody> call = retrofitService.voting(voto);
 
 
 
 
-        call.enqueue(new Callback<ResponseBody>() {
-            @Override
+                call.enqueue(new Callback<ResponseBody>() {
+                    @Override
 
 
-            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-                if (dialog.isShowing())
-                    dialog.dismiss();
-                if(response.code() == 200){
-                    Toast.makeText(getBaseContext(), "Voto Enviado com sucesso", Toast.LENGTH_SHORT).show();
-                }else{
-                    Toast.makeText(VotingActivity.this, "Não foi possível completar seu voto. Por favor, tente novamente." + response.code(), Toast.LENGTH_SHORT).show();
-                }
+                    public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+
+                        if(response.code() == 200){
+                            Toast.makeText(getBaseContext(), "Voto Enviado com sucesso", Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(VotingActivity.this, "Não foi possível completar seu voto. Por favor, tente novamente." + response.code(), Toast.LENGTH_SHORT).show();
+                        }
+                        Intent aboutIntent = new Intent(VotingActivity.this, MainActivity.class);
+                        startActivity(aboutIntent);
 
 
-            }
+                    }
 
-            @Override
-            public void onFailure(Call<ResponseBody> call, Throwable t) {
-                if (dialog.isShowing()) {
-                    dialog.dismiss();
-                    Toast.makeText(getBaseContext(), "Não foi possível fazer a conexão", Toast.LENGTH_SHORT).show();
-                }
+                    @Override
+                    public void onFailure(Call<ResponseBody> call, Throwable t) {
+
+                            Toast.makeText(getBaseContext(), "Não foi possível fazer a conexão", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+
+                dialog.dismiss();
             }
         });
+
+        builder.setNegativeButton("Não", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                // Do nothing
+                dialog.dismiss();
+            }
+        });
+
+        AlertDialog alert = builder.create();
+        alert.show();
+
+
     }
+
 }
